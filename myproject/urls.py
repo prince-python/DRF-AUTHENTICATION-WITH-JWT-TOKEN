697012c53c6593a3ffcc239f435e15dca91e486a
kali <kali@kali>
2024-05-21 12:00:11 +0530

    3commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,11 @@
 
+
+from django.contrib import admin
 from django.urls import path
 from myapp.views import ItemListCreateView, ItemDetailView
 
-urlpatterns += [
+urlpatterns= [
     path('api/items/', ItemListCreateView.as_view(), name='item-list-create'),
     path('api/items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+      path('admin/', admin.site.urls),
 ]
